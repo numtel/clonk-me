0a324083b9be862ca2c25daa47e122a9fea341a4
numtel <ben@latenightsketches.com>
2023-06-02 01:34:21 -0700

    Html string escape in solidity

---
+++ test/HtmlEscape.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import "../src/HtmlEscape.sol";
+
+contract HtmlEscapeTest is Test {
+  function testEscape() public {
+    HtmlEscape instance = new HtmlEscape();
+    string memory output = instance.escape("<a href=\"javascript:alert(`foo${'bar'}`)\">&quot;</a>");
+    assertEq(output, "&lt;a href=&quot;javascript:alert(&#96;foo${&#39;bar&#39;}&#96;)&quot;&gt;&amp;quot;&lt;/a&gt;");
+  }
+
+}
+
