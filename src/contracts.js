7b1a97a202ea58997ebd548f69346c66ec42f3f2
numtel <ben@latenightsketches.com>
2023-06-25 15:29:47 -0700

    Polygon mainnet deployment
    * Also, remove unused contracts/Ownable.sol

--- src/contracts.js
+++ src/contracts.js
@@ -1,8 +1,16 @@
 import messagesABI from './abi/Messages.json';
 
-export const defaultChain = 80001;
+export const defaultChain = 137;
 
 const byChain = {
+  137: { // polygon
+    messages: {
+      address: '0xcfa3756d600dba87dcf300556fb5143ce5d388b1',
+      abi: messagesABI,
+      chainId: 137,
+    },
+    root: '0xd961512c485f01fb972ff25de42a3097aa661140',
+  },
   80001: { // mumbai
     messages: {
       address: '0xa02E508f9D264bC9e73670038C955E9FC76a5C78',
